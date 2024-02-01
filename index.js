65e4ec689591e60dacffddf6e50189327ccf7a65
HardyLucifer <155144534+HardyLucifer@users.noreply.github.com>
2024-02-01 15:00:02 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const port = process.env.PORT;
+const headle = ()=>{
+fs.readFile("fileName", "utf-8",
+(err, data) => {
+ if (err) {
+     console.log(err);
+ } else {
+     res.writeHead (200, {
+         "content-type" : " text/html" });
+     res.write(data);
+     res.end;
+ };
+}) ;
+}
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+       handle("index.html", 200, req, res); 
+    }else if (req.url === "/about"){
+        handle("about.html", 200, req, res); 
+    }else if (req.url === "/contact"){
+        handle("contact.html", 200, req, res); 
+    }else {
+        handle("404.html", 404 , req, res); 
+    }
+});
+
+server.listen (port, () =>{
+    console.log(`Bhanchodd our server is running...`);
+});
